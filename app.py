439cae0dbc5286562a2452ffd7590118414336e1
Kunwar Luthera <kunwarluthera@Kunwars-MBP.frontierlocal.net>
2019-03-26 06:05:49 -0500

    new Updates testing

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ import redis
 
 
 
-ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
-SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+#ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
+#SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 REDIS_HOST= os.environ['REDIS_HOST']
 REDIS_PORT= os.environ['REDIS_PORT']
 
 def client_method(service,region):
-    client = boto3.client(service,aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY, region_name=region)
+    #client = boto3.client(service,'''aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY,''' region_name=region)
     return "received the "+ str(service) + str(region)
 
-client = boto3.client('s3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY, region_name='us-east-1')
-client_ec2 = boto3.client('ec2',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY, region_name='us-east-1')
+client = boto3.client('s3')#,'''aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY,''' region_name='us-east-1')
+client_ec2 = boto3.client('ec2')#,'''aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY,''' region_name='us-east-1')
 #print(client)
 app = Flask(__name__)
 
